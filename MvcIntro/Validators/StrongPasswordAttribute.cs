0ccfbaeb2308245fe08082b5e8e68f1010788db2
Matrix hunter <marybakayeva@gmail.com>
2022-05-16 15:16:29 +0300

    valid commit

---
+++ MvcIntro/Validators/StrongPasswordAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace MvcIntro.Validators
+{
+    public class StrongPasswordAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            string pattern =@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\]).{8,32}$";
+            if (!Regex.IsMatch(value.ToString(), pattern))return false;
+            return true;
+        }
+    }
+}
